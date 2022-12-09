fcecfbcf8774c14501af0b6594d704047624bbb2
khoinv <khoinv@smartosc.com>
2022-12-09 09:25:52 +0700

    doing

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 EKS_CLUSTER_NAME=eks-alb-2048game
+FARGATE_PROFILE=game2048
 
 # Sync time
 sudo service ntpd stop
@@ -97,9 +98,20 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-echo; echo "Lab is ready to use."
+source ~/.bash_profile
+
+echo; echo "Create Fargate profile"
+
+eksctl create fargateprofile  --cluster $EKS_CLUSTER_NAME --name $FARGATE_PROFILE --namespace $FARGATE_PROFILE
 
 
+echo; echo "Created Fargate profile $FARGATE_PROFILE"
 
+helm repo add eks https://aws.github.io/eks-charts
+helm repo update
 
+echo; echo "Added the Amazon EKS chart repo to Helm"
 
+kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
+
+echo; echo "Installed the TargetGroupBinding custom resource definitions"
